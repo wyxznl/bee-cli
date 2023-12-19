8ea972de40e449801e63a91bf2029bc063bdd014
wangyaxin04 <wangyaxin04@meituan.com>
2023-12-19 15:36:40 +0800

    fix: 把一个文件拆成多个文件,完善功能

--- core/init.js
+++ core/init.js
@@ -1,66 +1,26 @@
-const fs = require("fs");
 const path = require("path");
-const inquirer = require("inquirer");
+const initGitIgnore = require("../lib/initGitIgnore");
+const initNpmIgnoreFile = require("../lib/initNpmIgnore");
+const updatePackage = require("../lib/updatePackage");
+const selectEntryFile = require("../lib/selectEntryFile");
+const selectFramework = require("../lib/selectFramework");
+const inputFrameVersion = require("../lib/inputFrameVersion");
 
-// 创建 .gitignore 文件
-const initGitIgnoreData = `node_modules
-package.json
-package-lock.json
-README.md
-ironcode.json
-DEMO`;
-fs.writeFileSync(".gitignore", initGitIgnoreData);
-
-// 创建 .npmignore 文件
-const initNpmIgnoreData = `
-node_modules
-DEMO
-`;
-fs.writeFileSync(".npmignore", initNpmIgnoreData);
-
-// 创建并初始化 package.json 文件
 const curComponentPath = process.cwd();
 const componentName = path.basename(curComponentPath);
-// TODO -- 是不是应该去数据库中取？
-const obj = { id: 1 };
-const prefix = Buffer.from(JSON.stringify(obj)).toString("base64");
 
-var data = {
-  buildOptions: {
-    op: 0,
-  },
-  name: `@sfe/montage.${componentName}.${prefix}`,
-  virtualId: `${componentName}.${prefix}`,
-  _isCreate: true,
-};
-fs.writeFileSync("package.json", JSON.stringify(data, null, 2));
-
-// 询问用户入口文件是什么
-const files = [];
-function getAllFileFromPath(path) {
-  const res = fs.readdirSync(path);
-  for (let subPath of res) {
-    const statObj = fs.statSync(path + "/" + subPath);
-    if (statObj.isDirectory()) {
-      console.log("Dir:" + subPath);
-      getAllFileFromPath(path + "/" + subPath);
-    } else {
-      files.push(subPath);
-    }
+async function main() {
+  // TODO登录
+  initGitIgnore();
+  initNpmIgnoreFile();
+  updatePackage.init(componentName);
+  const selectedEntryFile = await selectEntryFile(curComponentPath);
+  updatePackage.updateByEntry(selectedEntryFile, componentName);
+  const curFrame = await selectFramework();
+  let curFrameVersion = "";
+  if (curFrame === "vue框架" || curFrame === "React框架") {
+    curFrameVersion = await inputFrameVersion(curFrame);
   }
+  console.log(curFrameVersion, 'curFrameVersion')
 }
-getAllFileFromPath(curComponentPath);
-// 与用户交互
-async function promptForFile() {
-  const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "file",
-      message: "请选择组件入口文件",
-      choices: files,
-    },
-  ]);
-  console.log("你选择的文件是：", answers.file);
-}
-
-promptForFile();
+main();
